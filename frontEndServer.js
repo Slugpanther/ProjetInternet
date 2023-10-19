39d0c6b63f4d7d2e7e71d74b85c845e892d7859b
Slugpanther <100293362+Slugpanther@users.noreply.github.com>
2023-10-19 14:06:08 -0400

    Create frontEndServer.js
    front end donner par prof

---
+++ frontEndServer.js
@@ -0,0 +1,9 @@
+const socket = new WebSocket('ws://localhost:9999');
+socket.onopen = function() {
+ console.log('Connecté au serveur WebSocket');
+};
+socket.onmessage = function(event) {
+ const message = document.createElement('div');
+ message.textContent = event.data;
+ document.getElementById('messages').appendChild(message);
+};
