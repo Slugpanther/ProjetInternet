1a6d13e8ece7f17a4456641bf55c3622cbffa712
TryAgain27 <126725473+TryAgain27@users.noreply.github.com>
2023-10-19 13:24:00 -0400

    Update Main.js
    Modification complete

--- Main.js
+++ Main.js
@@ -1,5 +1,28 @@
-const greeting = (name) => {
-    return `Bonjour, ${name} !`;
+import { fetchData, postTweet } from './network-functions.js';
+
+const username = "Antho1-2";
+
+const tweetButton = document.getElementById('tweet-button');
+const tweetInput = document.getElementById('tweet-input');
+const tweetList = document.getElementById('tweet-list');
+
+tweetButton.addEventListener('click', () => {
+  const tweetText = tweetInput.value;
+  if (tweetText) {
+    postTweet(username, tweetText);
+    tweetInput.value = '';
+  }
+});
+
+function updateTweetList() {
+  tweetList.innerHTML = '';
+  fetchData(username, (tweets) => {
+    tweets.forEach((tweet) => {
+      const tweetItem = document.createElement('div');
+      tweetItem.innerText = tweet.text;
+      tweetList.appendChild(tweetItem);
+    });
+  });
 }
-    const userName = "John";
-    console.log(greeting(userName));
+
+updateTweetList();
