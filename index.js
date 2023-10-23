dc024eb9c2ddae7bfffc6802b66967a21a1ab3e5
Slugpanther <100293362+Slugpanther@users.noreply.github.com>
2023-10-23 13:29:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 const http = require('http');
 const { dirname } = require('path');
 const server = http.createServer(app);
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
-});
-
 const io= new Server(server, {
     cors:{
         origin: "*",
@@ -14,6 +10,19 @@ const io= new Server(server, {
     }
 
 });
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket)=>{
+    console.log('Un client connecté');
+})
+socket.on('disconnect', ()=>{
+    console.log('Client est offline now');
+})
+
+
 
 server.listen(3000,()=> {
     console.log('Kill your server');
